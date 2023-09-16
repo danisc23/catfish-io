218b4604dd0be7fa0efa6071b642a929592b5fa4
Daniel <dani.sc23@gmail.com>
2023-09-16 13:23:49 +0200

    fix docker-compose redis dependeny, remove unused auth stuff

--- catfishio/core/auth/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class User(BaseModel):
-    id: str
-    picture: str
-    display_name: str
-    email: str
-    provider: str
--- catfishio/core/auth/tests/factories.py
+++
@@ -1,7 +0,0 @@
-from pydantic_factories import ModelFactory
-
-from catfishio.core.auth.models import User
-
-
-class UserFactory(ModelFactory):
-    __model__ = User
